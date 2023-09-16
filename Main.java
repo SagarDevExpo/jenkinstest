48e4e66957ed97249887dac043ac0fdd764dfae0
sagar nagireddi <sagar@Spirals-MacBook.local>
2023-09-15 23:36:38 -0400

    test java1 for jenkins

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int Arr[] = {1,2,3,4,5};
+        int sum= 0;
+        for(int i=0; i<Arr.length; i++){
+            sum += Arr[i];
+        }//for
+        System.out.println("Hello world!   "+ sum);
+    }
+}
